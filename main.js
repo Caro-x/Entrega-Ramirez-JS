c1a38fd2709fd677abf1edd536243360710cc32d
Carolina Ramirez <73265843+caro-ramirez@users.noreply.github.com>
2024-09-21 23:37:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,15 +7,15 @@ window.onload = function(){
                 throw new Error('Error al cargar el archivo JSON: ' + response.status);
             }
             return response.json();
-            })
-            .then(data => {
-                winePairings = data;
-                console.log('Datos cargados desde JSON:', winePairings); 
-                populateFoodSelect(); 
-                showPopularPairingsChart();
-            })
-            .catch(error => console.error('Error al cargar el archivo JSON:', error)); 
+        })
+        .then(data => {
+            winePairings = data;
+            console.log('Datos cargados desde JSON:', winePairings); 
+            populateFoodSelect(); 
+            showPopularPairingsChart();
             updateHistory(); 
+        })
+        .catch(error => console.error('Error al cargar el archivo JSON:', error)); 
 };
 
 const foodSelect = document.getElementById('food');
@@ -26,7 +26,7 @@ function populateFoodSelect() {
         console.error('Elemento select no encontrado');
         return;
     }
-    
+
     Object.keys(winePairings).forEach(food => {
         const option = document.createElement('option');
         option.value = food;
@@ -34,7 +34,6 @@ function populateFoodSelect() {
         foodSelect.appendChild(option);
     });
 }
-
 
 function concatenar(...args) {
     return args.join(''); 
@@ -57,19 +56,19 @@ function suggestWine() {
 
         wineRecommendations.innerHTML = `<p>${recommendationText}</p>`;
 
-        const history = JSON.parse(localStorage.getItem('recommendationHistory')) || [];
-        history.push({
-            food: food,
-            wine: recommendations[0].name,
-            timestamp: new Date().getTime() 
-        });
-        localStorage.setItem('recommendationHistory', JSON.stringify(history));
+        const history = JSON.parse(localStorage.getItem('recommendationHistory')) || []; 
 
         const popularPairings = JSON.parse(localStorage.getItem('popularPairings')) || {};
         const pairingKey = `${food} - ${recommendations[0].name}`; 
         popularPairings[pairingKey] = (popularPairings[pairingKey] || 0) + 1;
         localStorage.setItem('popularPairings', JSON.stringify(popularPairings));
 
+        history.push({
+            food: food,
+            wine: recommendations[0].name,
+            timestamp: new Date().getTime() 
+        });
+        localStorage.setItem('recommendationHistory', JSON.stringify(history));
         updateHistory();
         showPopularPairingsChart();
     } 
@@ -77,10 +76,6 @@ function suggestWine() {
         wineRecommendations.innerHTML = "No tenemos recomendaciones para esa comida."; 
     }
 }
-
-const popularPairings = JSON.parse(localStorage.getItem('popularPairings')) || {};
-popularPairings[food] = (popularPairings[food] || 0) + 1;
-localStorage.setItem('popularPairings', JSON.stringify(popularPairings));
 
 function clearRecommendations() {
     wineRecommendations.innerHTML = ''; 
@@ -90,7 +85,6 @@ function clearHistory() {
     localStorage.removeItem('recommendationHistory');
     updateHistory(); 
 }
-
 
 function updateHistory() {
     const historyList = document.getElementById('recommendationHistory');
@@ -104,22 +98,21 @@ function updateHistory() {
         history.forEach(entry => {
             const listItem = document.createElement('li');
             const formattedDate = new Date(entry.timestamp).toLocaleString(); 
-            listItem.textContent = `Comida: ${entry.food}, Vino: ${entry.wine} (${formattedDate})`;
+            listItem.textContent = `Comida: ${entry.food}, Vino: <span class="math-inline">\{entry\.wine\} \(</span>{formattedDate})`;
             historyList.appendChild(listItem);
         });
     }
 }
 
-let myChart;
+let myChart; 
 
 function showPopularPairingsChart() {
     if (myChart) {
         myChart.destroy(); 
     }
-    
 
     const popularPairings = JSON.parse(localStorage.getItem('popularPairings')) || {};
-    const pairingsData = Object.entries(JSON.parse(localStorage.getItem('popularPairings')) || {});    
+    const pairingsData = Object.entries(popularPairings);
     pairingsData.sort((a, b) => b[1] - a[1]); 
 
     const chartData = {
@@ -138,17 +131,6 @@ function showPopularPairingsChart() {
         data: chartData,
         options: {
             scales: {
-                x: {
-                    ticks: {
-                        callback: function(value, index, values) {
-                            if (value.length > 20) {
-                                return value.substring(0, 20) + '...';
-                            } else {
-                                return value;
-                            }
-                        }
-                    }
-                },
                 y: {
                     beginAtZero: true
                 }
@@ -159,7 +141,7 @@ function showPopularPairingsChart() {
                         label: function(context) {
                             const [food, wine] = context.label.split(' - ');
                             const wineInfo = winePairings[food] && winePairings[food].find(w => w.name === wine);
-                            return `${food} + ${wine} (${wineInfo ? wineInfo.description : ''})`; 
+                            return `${food} + <span class="math-inline">\{wine\} \(</span>{wineInfo ? wineInfo.description : ''})`; 
                         }
                     }
                 }
@@ -171,26 +153,25 @@ function showPopularPairingsChart() {
     myChart = new Chart(wineTypeChartCanvas, chartConfig);
 }
 
-function generarColores(cantidad) {
+function generarColores(cantidad, opacidad = 0.2) {
     const coloresSuaves = [
-        'rgba(255, 192, 203, 0.8)',  // Rosa suave
-        'rgba(135, 206, 250, 0.8)',  // Azul cielo suave
-        'rgba(152, 251, 152, 0.8)',  // Verde claro suave
-        'rgba(255, 255, 102, 0.8)',  // Amarillo suave
-        'rgba(250, 128, 114, 0.8)',  // Salmón suave
-        'rgba(173, 216, 230, 0.8)',  // Azul claro suave
-        'rgba(240, 230, 140, 0.8)',  // Amarillo pálido
-        'rgba(210, 105, 30, 0.8)',   // Chocolate 
-        'rgba(138, 43, 226, 0.8)',   // Azul violeta
-        'rgba(34, 139, 34, 0.8)',    // Verde bosque
-        'rgba(255, 160, 122, 0.8)',  // Durazno claro
-        'rgba(221, 160, 221, 0.8)',  // Ciruela
-        'rgba(0, 128, 128, 0.8)'     // Verde azulado
+        'rgba(255, 192, 203, 0.8)',  
+        'rgba(135, 206, 250, 0.8)',  
+        'rgba(152, 251, 152, 0.8)',  
+        'rgba(255, 255, 102, 0.8)', 
+        'rgba(250, 128, 114, 0.8)',  
+        'rgba(173, 216, 230, 0.8)', 
+        'rgba(240, 230, 140, 0.8)',  
+        'rgba(210, 105, 30, 0.8)',   
+        'rgba(138, 43, 226, 0.8)',   
+        'rgba(34, 139, 34, 0.8)',   
+        'rgba(255, 160, 122, 0.8)', 
+        'rgba(221, 160, 221, 0.8)', 
+        'rgba(0, 128, 128, 0.8)'    
     ];
 
     while (coloresSuaves.length < cantidad) {
         coloresSuaves.push(...coloresSuaves); 
     }
 
-    return coloresSuaves.slice(0, cantidad); 
-}
+    return coloresSuaves.slice(0, cantidad);
